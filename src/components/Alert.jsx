78b1d4cd5b70406fdb66527e4bc193f0af718251
geetika-rathore <geetikarathore370@gmail.com>
2024-02-17 13:33:14 +0530

    change saved

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Alert = ({ message, phoneNumber }) => {
   return (
-    <div className="bg-[#f2c196] text-yellow-800 border border-red-500 fixed top-0 left-0 w-full z-50 px-4 py-2 rounded-md text-center font-semibold  shadow-md">
-      {message} <a href={`tel:${phoneNumber}`} className=""> {phoneNumber}</a>
+    <div className="bg-[#f2c196] text-yellow-800 border border-red-500 fixed top-0 left-0 w-full z-50 px-4 py-2 rounded-md text-center font-semibold shadow-md">
+      {message} <a href={`tel:${phoneNumber}`} className="text-sm px-2 py-1">{phoneNumber}</a>
     </div>
   );
 };
