c8b760b430cb006141b04b4c42533943da28b597
geetika rathore <110020464+geetika-rathore@users.noreply.github.com>
2024-07-19 06:46:53 +0530

    Update MenuItems.jsx

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -15,7 +15,7 @@ const MenuItems = () => {
         window.open(whatsappUrl, '_blank');
       };
   return (
-    <div className="container mx-auto px-5 py-8 ">
+    <div className="container mx-auto px-5 py-8 pt-2 ">
       <h2 className="text-3xl font-bold mb-8 text-center">Menu Items</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {/* Item 1 */}
